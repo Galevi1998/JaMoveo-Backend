4b9f4828623977e8756fb2075e4cf0f3c05474d8
Galevi1998 <135505755+Galevi1998@users.noreply.github.com>
2025-06-24 22:21:06 +0300

    final product

--- serverjs/controllers/authController.js
+++ serverjs/controllers/authController.js
@@ -78,13 +78,13 @@ exports.register = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "Lax" : "Lax",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
         maxAge: 15 * 60 * 1000,
       })
       .cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "Lax" : "Lax",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .status(201)
